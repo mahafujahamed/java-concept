832d8eaa5ffa5712300d54c5a1c179b13dd8de5c
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-03-10 00:14:50 +0600

    create class and object

---
+++ Class.java
@@ -0,0 +1,8 @@
+public class Class { // To create class name and file name same 
+    // create  an objects called "myObj"
+    int num = 45;    // int type variable. variable name. assignment operator and value 
+    public static void main (String[] args){
+        Class myObj = new Class();    //call the object
+        System.out.println(myObj.num); //output- 45
+    }
+}
